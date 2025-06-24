a3b7a6620ff5071905ca0d08f0f920f79f136cb1
c0a2419229 <c0a2419229@edu.teu.ac.jp>
2025-06-24 16:52:13 +0900

    演習２：複数ビーム

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -176,13 +176,15 @@ def main():
     clock = pg.time.Clock()
     tmr = 0
     score = Score(0)
+    beams = []
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 # スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)            
+                beam = Beam(bird)       
+                beams.append(beam)     
         screen.blit(bg_img, [0, 0])
         
         for bomb in bombs:
@@ -197,19 +199,23 @@ def main():
                 return
 
         for i, bomb in enumerate(bombs):
-            if beam is not None:
+            # if beam is not None:
+            for j, beam in enumerate(beams):
                 if beam.rct.colliderect(bomb.rct):
                     # 爆弾を撃ち落としたときに爆弾とビームを消す
-                    beam = None
+                    beams[j] = None
                     bombs[i] = None
                     bird.change_img(6, screen)  # よろこぶこうかとんに画像変更
                     score.score += 1
+            beams = [beam for beam in beams if beam is not None]
         bombs = [bomb for bomb in bombs if bomb is not None]
         score.update(screen)
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        if beam is not None:
+        for beam in beams:
             beam.update(screen)
+            if check_bound(beam.rct) != (True, True):
+                beam = None 
         for bomb in bombs:
             bomb.update(screen)
         pg.display.update()
